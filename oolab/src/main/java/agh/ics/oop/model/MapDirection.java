615e66fff7f4023b8f666c7f0ead6ee07b78c8d3
Kinga Bunkowska <tcx7@o2.pl>
2023-10-27 10:28:54 +0200

    Change MapDirection to increase readibility

--- oolab/src/main/java/agh/ics/oop/model/MapDirection.java
+++ oolab/src/main/java/agh/ics/oop/model/MapDirection.java
@@ -1,36 +1,33 @@
 package agh.ics.oop.model;
 
 public enum MapDirection {
-    NORTH(new Vector2d(0,1)),
-    EAST(new Vector2d(1,0)),
-    SOUTH(new Vector2d(0,-1)),
-    WEST (new Vector2d(-1,0));
+    NORTH(new Vector2d(0, 1), "Północ"),
+    EAST(new Vector2d(1, 0), "Wschód"),
+    SOUTH(new Vector2d(0, -1), "Południe"),
+    WEST(new Vector2d(-1, 0), "Zachód");
 
     final private Vector2d unitValue;
-    private MapDirection(Vector2d vector2d) {
+    final private String string;
+
+    private MapDirection(Vector2d vector2d, String string) {
         this.unitValue = vector2d;
+        this.string = string;
     }
 
     public String toString() {
-        return switch(this){
-            case NORTH -> "Północ";
-            case SOUTH -> "Południe";
-            case EAST -> "Wschód";
-            case WEST -> "Zachód";
-        };
+        return this.string
     }
 
-    public Vector2d toUnitVector(){
+    public Vector2d toUnitVector() {
         return this.unitValue;
     }
 
-    public MapDirection next(){
-        return MapDirection.values()[(this.ordinal()+1!=MapDirection.values().length)?this.ordinal()+1:0];
+    public MapDirection next() {
+        return MapDirection.values()[(this.ordinal() + 1) % MapDirection.values().length];
     }
 
-    public MapDirection previous(){
-        return MapDirection.values()[(this.ordinal()-1>=0)?this.ordinal()-1:MapDirection.values().length-1];
+    public MapDirection previous() {
+        return MapDirection.values()[(this.ordinal() + 3) % MapDirection.values().length];
     }
-
 
 }
